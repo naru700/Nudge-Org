8a9816c9b0d3ce72775378db59801fab8f0f1e54
naru700 <youremail@domain.com>
2025-07-14 22:35:56 -0500

    Nudge-Home: fixed merge conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { SessionComponent } from './session/session.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './guards/auth.gaurd';
+import { AuthRequiredGuard } from './guards/auth-required.gaurd';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'session', component: SessionComponent },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthRequiredGuard] },
+  { path: 'session', component: SessionComponent, canActivate: [AuthRequiredGuard] },
   { path: '**', redirectTo: 'login' },
 ];
 
