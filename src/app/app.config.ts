97144c78e55920b3860f467da983a28fea6534d5
naru700 <youremail@domain.com>
2025-07-29 18:01:26 -0500

    fixed merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { AuthEffects } from './auth/store/auth.effects';
 import { authReducer } from './auth/store/auth.reducer';
@@ -13,19 +11,9 @@ import { authReducer } from './auth/store/auth.reducer';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withJsonpSupport()),
+    provideHttpClient(),
     provideAnimations(),
     provideStore({ auth: authReducer }),
     provideEffects([AuthEffects]),
-    // Only include DevTools in development mode
-    ...(!environment.production ? [
-      provideStoreDevtools({
-        maxAge: 25,
-        logOnly: environment.production,
-        autoPause: true,
-        trace: false,
-        traceLimit: 75
-      })
-    ] : [])
   ]
 };
