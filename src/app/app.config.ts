7b7c0332605c00414592ad1f77b888ebf6bd2a0d
naru700 <youremail@domain.com>
2025-07-29 17:31:32 -0500

    fixed nav bar issues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,31 @@
+import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideStore } from '@ngrx/store';
-import { routes } from './app.routes';
-import { authReducer } from './auth/store/auth.reducer';
-import { AuthEffects } from './auth/store/auth.effects';
+import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+import { AuthEffects } from './auth/store/auth.effects';
+import { authReducer } from './auth/store/auth.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withJsonpSupport()),
     provideAnimations(),
-    provideStore({ auth: authReducer }),  
+    provideStore({ auth: authReducer }),
     provideEffects([AuthEffects]),
+    // Only include DevTools in development mode
+    ...(!environment.production ? [
+      provideStoreDevtools({
+        maxAge: 25,
+        logOnly: environment.production,
+        autoPause: true,
+        trace: false,
+        traceLimit: 75
+      })
+    ] : [])
   ]
 };
